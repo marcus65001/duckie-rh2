992e95d32febfea767214aa091a65d90ba4ee96c
Marcus Huang <marcus65001@gmail.com>
2023-01-20 15:35:28 -0700

    exercise 12

--- launchers/default.sh
+++ launchers/default.sh
@@ -14,6 +14,7 @@ dt-launchfile-init
 
 # launching app
 dt-exec python3 -m "hello.hello"
+dt-exec python3 -m "hello.numpy_test"
 
 
 # ----------------------------------------------------------------------------
---
+++ packages/hello/numpy_test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+
+print("Testing numpy: {}".format(np.array([1,2]).sum()))
